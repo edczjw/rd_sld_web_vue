f574ed6cf0cf287e7ef3b5f0560d2445c5668e76
edczjw <347350971@qq.com>
2019-07-22 10:23:34 +0800

    first commit

---
+++ SHULOUDAI/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+/* 整体布局样式 */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: '黑体';
+
+  text-decoration: none;
+}
+
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+</style>
